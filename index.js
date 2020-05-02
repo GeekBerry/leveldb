ea42f7617a07710bcc311755db9411ee4eab21de
Wanghaizhou <geekberry@qq.com>
2020-05-03 02:18:11 +0800

    ADD: support both level-up, level-down base class

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-module.exports = require('./src/LevelUp');
+const LevelDown = require('./src/LevelDown');
+const LevelUp = require('./src/LevelUp');
+
+/**
+ * @param options
+ * @param options.location {string} - The dir of database
+ * @param [options.LevelUp] - Create LevelDB from level-down class, such as `level`, `level-party`
+ * @param [options.LevelDown=require('leveldown')] - Create LevelDB from level-down class, such as `leveldown`, `rocksdb`
+ * @return {LevelDB}
+ */
+function LevelDB(options) {
+  if (options.LevelUp) {
+    return new LevelUp(options);
+  } else {
+    return new LevelDown(options);
+  }
+}
+
+module.exports = LevelDB;
 module.exports.Interface = require('./src/LevelInterface');
 module.exports.Server = require('./src/Server');
 module.exports.Client = require('./src/Client');
--- src/LevelUp.js
+++ src/LevelUp.js
@@ -1,120 +1,56 @@
-const { promisify } = require('util');
-const LevelDBLevelDown = require('leveldown');
 const LevelInterface = require('./LevelInterface');
-const { promisifyResults } = require('./util');
 
-class LevelUP extends LevelInterface {
-  /**
-   * @param options {object}
-   * @param options.location {string}
-   * @param [options.LevelDown=`require('leveldown')`] {object} - `require('leveldown')` or `require('rocksdb')`
-   * @param [options.asBuffer=false] {string}
-   * @param [options.cacheGet=true] {boolean}
-   * @param [options.cacheIter=false] {boolean}
-   * @param [options.syncPut=false] {boolean}
-   * @param [options.syncDel=false] {boolean}
-   * @param [options.syncBatch=false] {boolean}
-   * @param [rest.errorIfExists=false] {boolean}
-   * @param [rest.createIfMissing=true] {boolean}
-   * @param [rest.compression=true] {boolean}
-   * @param [rest.readOnly=false] {boolean}
-   * @param [rest.cacheSize=8*1024*1024] {number}
-   */
+/**
+ * extend LevelInterface by level-up
+ */
+class LevelDB extends LevelInterface {
   constructor({
-    LevelDown,
+    LevelUp,
     location,
-    asBuffer = false,
-    cacheGet = true,
-    cacheIter = false,
-    syncPut = false,
-    syncDel = false,
-    syncBatch = false,
-    ...rest
-  } = {}) {
+    ...options
+  }) {
     super();
-
-    const levelDown = LevelDown === undefined ? new LevelDBLevelDown(location) : new LevelDown(location);
-    levelDown.open = promisify(levelDown.open);
-    levelDown.get = promisify(levelDown.get);
-    levelDown.batch = promisify(levelDown.batch);
-    levelDown.put = promisify(levelDown.put);
-    levelDown.del = promisify(levelDown.del);
-    levelDown.clear = promisify(levelDown.clear);
-    levelDown.close = promisify(levelDown.close);
-    this.levelDown = levelDown.open(rest).then(() => levelDown);
-
-    this.iterOptions = { fillCache: cacheIter, keyAsBuffer: asBuffer, valueAsBuffer: asBuffer };
-    this.getOptions = { fillCache: cacheGet, asBuffer };
-    this.putOptions = { sync: syncPut };
-    this.delOptions = { sync: syncDel };
-    this.batchOptions = { sync: syncBatch };
+    const levelUp = new LevelUp(location, options);
+    this.put = levelUp.put.bind(levelUp);
+    this.del = levelUp.del.bind(levelUp);
+    this.batch = levelUp.batch.bind(levelUp);
+    this.clear = levelUp.clear.bind(levelUp);
+    this.close = levelUp.close.bind(levelUp);
+    this.levelUp = levelUp;
   }
 
-  async put(key, value) {
-    const levelDown = await this.levelDown;
-    return levelDown.put(key, value, this.putOptions);
-  }
-
-  async del(key) {
-    const levelDown = await this.levelDown;
-    return levelDown.del(key, this.delOptions);
-  }
-
-  async batch(array) {
-    const levelDown = await this.levelDown;
-
-    return levelDown.batch(array, this.batchOptions);
-  }
-
-  async clear(options) {
-    const levelDown = await this.levelDown;
-    return levelDown.clear(options);
-  }
-
-  // --------------------------------------------------------------------------
   async get(key) {
-    const levelDown = await this.levelDown;
     try {
-      return await levelDown.get(key, this.getOptions);
+      return await this.levelUp.get(key);
     } catch (e) {
-      if (e.message.startsWith('NotFound:')) {
+      if (e.notFound) {
         return undefined;
       }
       throw e;
     }
   }
 
-  async list(filter = {}) {
-    const levelDown = await this.levelDown;
-
-    const iter = levelDown.iterator({ ...this.iterOptions, ...filter });
-    iter.end = promisify(iter.end);
-    iter.next = promisifyResults(iter.next);
-
-    const array = [];
-    for (let pair = await iter.next(); pair.length; pair = await iter.next()) {
-      const [key, value] = pair;
-      array.push({ key, value });
-    }
-    await iter.end();
-    return array;
+  async list(options) {
+    return new Promise(((resolve, reject) => {
+      const array = [];
+      this.levelUp.readStream(options)
+        .on('data', data => {
+          array.push(data);
+        })
+        .on('end', () => {
+          resolve(array);
+        })
+        .on('error', reject);
+    }));
   }
 
   async keys(filter = {}) {
-    const array = await this.list({ ...filter, values: false });
-    return array.map(each => each.key);
+    return this.list({ ...filter, values: false });
   }
 
   async values(filter = {}) {
-    const array = await this.list({ ...filter, keys: false });
-    return array.map(each => each.value);
-  }
-
-  // --------------------------------------------------------------------------
-  async close() {
-    const levelDown = await this.levelDown;
-    return levelDown.close();
+    return this.list({ ...filter, keys: false });
   }
 }
 
-module.exports = LevelUP;
+module.exports = LevelDB;
